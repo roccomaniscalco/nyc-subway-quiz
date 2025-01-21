098e16ccc52001545ad097032326ccd69b6612cc
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2025-01-20 22:13:40 -0500

    change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,5 @@
 import type { Metadata } from "next";
-import { Inter_Tight } from "next/font/google";
 import "./globals.css";
-
-const interTight = Inter_Tight({
-  variable: "--font-inter-tight",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "NYC Subway Quiz",
@@ -18,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${interTight.variable}`}>
-      <body className={"font-[family-name:var(--font-inter-tight)] antialiased"}>{children}</body>
+    <html lang="en">
+      <body className={"antialiased"}>{children}</body>
     </html>
   );
 }
